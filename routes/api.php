d872848a3ad1845ae04a9d855e78a873be799525
Zakariae Lajoui <lajoui.zakariae.1@gmail.com>
2024-10-24 14:16:37 +0100

    fix: api namespace

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\StoreController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request): mixed {
     return $request->user();
 });
+
+Route::apiResource('categories', CategoryController::class);
+
+Route::delete('categories', [CategoryController::class, 'destroyMultiple']);
+
+Route::apiResource('stores', StoreController::class);
+
+Route::delete('stores', [StoreController::class, 'destroyMultiple']);
